0294d84917cf9074b8b8e8bce1306f048782586a
Gailalera <cit183e-17@M303.UC-BCF.EDU.PH>
2023-03-07 17:56:06 +0800

    first commit

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Customer;
+use DB;
+
+class CustomerController extends Controller
+{
+    public function index()
+    {
+        $data = Customer::all();
+        return view('customer.index',['customers'=>$data]);
+    } 
+
+    public function delete($id){
+        $delete=DB::table("customers")
+        ->where("id","=",$id)
+        ->delete();
+        return redirect('/')->with('success','customer deleted');
+    }
+   
+}
---
+++ resources/views/customer/index.blade.php
@@ -0,0 +1,72 @@
+@include('partials.header')
+<x-nav/>
+
+<div class="overflow-hidden rounded-lg border border-gray-200 shadow-md m-5">
+    <table class="w-full border-collapse bg-white text-left text-sm text-gray-500">
+      <thead class="bg-gray-50">
+        <tr>
+          <th scope="col" class="px-6 py-4 font-medium text-gray-900">Last Name</th>
+          <th scope="col" class="px-6 py-4 font-medium text-gray-900">First Name</th>
+          <th scope="col" class="px-6 py-4 font-medium text-gray-900">email</th>
+          <th scope="col" class="px-6 py-4 font-medium text-gray-900">Address</th>
+          <th scope="col" class="px-6 py-4 font-medium text-gray-900"></th>
+        </tr>
+      </thead>
+      <tbody class="divide-y divide-gray-100 border-t border-gray-100">
+      @foreach ($customers as $customer)
+      <tr class="hover:bg-gray-50">
+          <th class="flex gap-3 px-6 py-4 font-normal text-gray-900">
+            
+            <div class="text-sm">
+              
+              <div class="font-medium text-gray-700">{{$customer->lastName}}</div>
+              </div>
+          </th>
+  
+          <td class="px-6 py-4">
+          <div class="text-sm">
+              <div class="font-medium text-gray-700">{{$customer->firstName}}</div>
+              </div>
+          </td>
+  
+          <td class="px-6 py-4">
+          <div class="text-sm">
+              <div class="font-medium text-gray-700">{{$customer->email}}</div>
+              </div>
+          </td>
+  
+          <td class="px-6 py-4">
+          <div class="text-sm">
+              <div class="font-medium text-gray-700">{{$customer->address}}</div>
+              </div>
+          </td>
+  
+  
+          <td class="px-6 py-4">
+            <div class="flex justify-end gap-4">
+              <a x-data="{ tooltip: 'Edit' }" href="/">
+              Edit
+              </a>
+              </div>
+          </td>
+
+          <td class="px-6 py-4">
+            <div class="flex justify-end gap-4">
+              <a x-data="{ tooltip: 'Delete' }" href="delete/{{$customer->id}}">
+                
+               Delete
+              </a>
+            </div>
+          </td>
+       
+      
+
+          @endforeach
+  </tr>
+      </tbody>
+     
+     </table>
+  </div>
+
+
+  @include('partials.footer')
